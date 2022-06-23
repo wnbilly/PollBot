ee29e16b1aee7dc04d0d8d1d0a7e7622545118ca
wnbilly <weynansbilly@gmail.com>
2022-06-23 19:41:08 +0200

    improved display, refresh not working

--- pollBot.py
+++ pollBot.py
@@ -7,9 +7,10 @@ import random
 
 import discord
 from discord.ext import commands
-from discord import option
+from discord import option, InteractionMessage
 from discord.ui import Button, View
 
+from pollBot_display import percentage_display
 
 TOKEN = "OTg5NDMzNTc5NzczNzc1OTAy.GlW4V9.9q1SrWh2h9qLu0OmPbTuwqeL7BgEFKhrm96p5I"
 
@@ -19,6 +20,7 @@ async def on_ready():
     print(f"PollBot ready via {bot.user}.")
 
 # note : option_type = 3 for string, 4 for integer, 5 for boolean
+
 
 # poll command, callable via /poll "question" "answer1" "answer2"
 @bot.slash_command(name="poll", description="Create a poll")
@@ -34,14 +36,46 @@ async def test(
     answer2: str,
     display: int,
 ):
-    
+
+    async def answer_button_callback(interaction):
+        await interaction.response.send_message("Button ")
+        nb_answer1 += 1
+
+    async def answer2_button_callback(interaction):
+        await interaction.response.send_message("Button ")
+        await print("DESO POUR LE SPAM, HESITEZ PAS A MUTER LE CHANNEL")
+
+
+
+    async def refresh_display(interaction):
+        percentages = [random.randint(0,100)/100, random.randint(0,100)/100, random.randint(0,100)/100] # from 0 to 1
+        percentage_display(percentages)
+        await InteractionMessage.edit_original_message(file=discord.File('barChart.png'), content="Last update at " +str(time.strftime('%X'))+ "," + str(time.strftime('%x')))
+        await ctx.send(file=discord.File('barChart.png'), content="Last update at " +str(time.strftime('%X'))+ "," + str(time.strftime('%x')))
+
+    # refresh button
+    button_refresh_display = Button(label="Refresh Display", style=discord.ButtonStyle.red)
+    button_refresh_display.callback = refresh_display
+
+    display_view = View()
+    display_view.add_item(button_refresh_display)
+
+    # create and display the choice buttons
     button1 = Button(label="1 : " + answer1, style=discord.ButtonStyle.blurple)
+    button1.callback = answer_button_callback
+
     button2 = Button(label="2 : " + answer2, style=discord.ButtonStyle.blurple)
-    button_refresh_display = Button(label="Refresh Display" + answer2, style=discord.ButtonStyle.red)
+    button1.callback = answer2_button_callback
+
     buttons_view = View()
     buttons_view.add_item(button1)
     buttons_view.add_item(button2)
-    buttons_view.add_item(button_refresh_display)
+    
     await ctx.send("Question : " + question, view=buttons_view)
+
+    # image update + 1st display
+    percentages = [random.randint(0,100)/100, random.randint(0,100)/100, random.randint(0,100)/100] # from 0 to 1
+    percentage_display(percentages)
+    await ctx.send("Last update at " +str(time.strftime('%X'))+ "," + str(time.strftime('%x')), file=discord.File('barChart.png'), view=display_view)
 
 bot.run(TOKEN)
